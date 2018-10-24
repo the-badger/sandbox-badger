d69b33e717740239efb7d366939279f1d3eeb34c
Olivier Soulet <olivier.soulet@akeneo.com>
2018-10-24 13:51:10 +0200

    Introduce behat

--- src/SharedSpace/Application/Kernel.php
+++ src/SharedSpace/Application/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Badger;
+namespace Badger\SharedSpace\Application;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
